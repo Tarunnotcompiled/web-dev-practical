bad0770be57331cb487fb9cb266f6a27f64de6b6
TARUN <raot98861@gmail.com>
2026-05-08 11:16:12 +0530

    Update App.jsx

--- practical/src/App.jsx
+++ practical/src/App.jsx
@@ -1,18 +1,14 @@
-import React from "react"
+import React from "react";
 import Counter from "./component/Counter";
 import ContextDemo from "./component/ContexDemo";
+
 function App() {
   return (
     <div>
       <Counter />
+      <ContextDemo />
     </div>
   );
 }
-function App(){
-  return(
-    <div>
-      <ContextDemo/>
-    </div>
-  );
-}
+
 export default App;
